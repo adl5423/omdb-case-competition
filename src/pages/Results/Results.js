20f717cd462523a806f06354340a8d65ab97cf94
anesepark <anesepark@gmail.com>
2021-08-10 16:45:51 -0400

    Created results page, updated some stylings for landing and defaults

--- src/pages/Results/Results.js
+++ src/pages/Results/Results.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import Card from '../../components/Card'
+import Card from '../../components/Card/Card'
+
+import './index.scss';
+import '../../styles/index.scss'
 
 function Results() {
     return (
-        <div>
-            <h1>looking for smthn?</h1>
+        <div class="results-page">
+            <h1>Looking for something?</h1>
             <input type="text" id="searchField" name="searchField" />
             <Card />
             <Card />
             <Card />
+            <Card />
+
+            <a id="scroll-btn" href="#">Scroll to top</a>
         </div>
     );
 }
